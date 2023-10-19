8213fa5352efc15d4d5cab3309407226231719d2
David Alejandro Orozco Casas <ao7417331@gmail.com>
2023-10-18 20:43:48 -0500

    styles

--- pages/index.js
+++ pages/index.js
@@ -50,12 +50,9 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl font-mono ">
-              David Orozco
-            </h2>
-            <h3 className="text-2xl py-2 md:text-3xl2 font-mono dark:text-white">
+            <h1 className="text-2xl py-2 md:text-3xl2 font-mono dark:text-white">
               Systems enginner and FullStack Developer.
-            </h3>
+            </h1>
             <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto font-mono dark:text-white">
             Im passionate about programming and tackling technical challenges. I enjoy exploring new technologies and seeking innovative solutions
             </p>
@@ -74,62 +71,53 @@ export default function Home() {
           </div>
         </section>
         <section> 
-          <div className="dark:text-gray-300 mt-8">
-            <h3 className="text-3xl py-1">Services i offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">
-              my services
-              <span className="text-teal-500">agencies</span>
-              consulted for <span className="text-teal-500">startups</span>
-              and collaborated wih talanted people to create digital for both
-              business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800">
-              i am systems enginner and fullStack Develop with tech skills
-              in javaScript, ReactJs, NodeJs, SQL, HTML and CSS
+          <div className="dark:text-gray-300 mt-8 text-center">
+            <h3 className="text-3xl py-1  max-w-xl mx-auto font-mono">Hi! I am David</h3>
+            <p className="text-center py-2 leading-8 text-gray-800  md:text-xl max-w-xl mx-auto font-mono dark:text-gray-300">
+            I am a passionate FullStack developer dedicated to building robust and scalable solutions. My fascination with technology 
+            and development began at the age of 19, igniting a deep passion for programming within me. Since then, I have focused my 
+            career on systems engineering and engage in projects that allow me to continue growing as a programmer.
+
+            My true passion lies in finding solutions to real-life situations through code. I am committed to tackling challenges 
+            and creating practical, efficient solutions that benefit both individuals and businesses.
             </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-center items-center flex flex-col shadow-lg p-10 rounded-xl my-10 bg-gradient-to-b from-teal-500 ">
+            <div className="text-center items-center flex flex-col shadow-lg p-10 rounded-xl my-10 bg-gradient-to-b from-teal-500 dark:text-gray-300 ">
               <Image src={design} width={100} height={100} alt=""/>
-              <h3 className="text-lg font-medium pt-8 pb-2">Bautiful designs</h3>
-              <p className="py-2">
-                Creating elegant desing suited for your needs. 
-              </p>
+              <h3 className="text-lg font-medium pt-8 pb-2">Front-End</h3>
               <h4 className="py-4 text-teal-600">Desing tools i use</h4>
-              <p className="text-gray-800 py-1">JavaScript</p>
-              <p className="text-gray-800 py-1">NodeJs</p>
-              <p className="text-gray-800 py-1">ReactJs</p>
-              <p className="text-gray-800 py-1">SQL</p>
-              <p className="text-gray-800 py-1">HTML</p>
-              <p className="text-gray-800 py-1">CSS</p>  
+              <ul>
+                <li>CSS</li>
+                <li>Tailwin</li>
+                <li>Boostrap</li>
+                <li>HTML</li>
+                <li>Slim</li>
+              </ul>
             </div>
             <div className="text-center items-center flex flex-col shadow-lg p-10 rounded-xl my-10 bg-gradient-to-b from-teal-500">
               <Image src={consulting} width={100} height={100} alt=""/>
-              <h3 className="text-lg font-medium pt-8 pb-2">Bautiful designs</h3>
-              <p className="py-2">
-                Creating elegant desing suited for your needs. 
-              </p>
+              <h3 className="text-lg font-medium pt-8 pb-2">Back-End</h3>
               <h4 className="py-4 text-teal-600">Desing tools i use</h4>
-              <p className="text-gray-800 py-1">JavaScript</p>
-              <p className="text-gray-800 py-1">NodeJs</p>
-              <p className="text-gray-800 py-1">ReactJs</p>
-              <p className="text-gray-800 py-1">SQL</p>
-              <p className="text-gray-800 py-1">HTML</p>
-              <p className="text-gray-800 py-1">CSS</p>  
+              <ul>
+                <li>CSS</li>
+                <li>Tailwin</li>
+                <li>Boostrap</li>
+                <li>HTML</li>
+                <li>Slim</li>
+              </ul>
             </div>
             <div className="text-center items-center flex flex-col shadow-lg p-10 rounded-xl my-10 bg-gradient-to-b from-teal-500" >
               <Image src={code} width={100} height={100} alt=""/>
-              <h3 className="text-lg font-medium pt-8 pb-2">Bautiful designs</h3>
-              <p className="py-2">
-                Creating elegant desing suited for your needs. 
-              </p>
+              <h3 className="text-lg font-medium pt-8 pb-2">Data Base</h3>
               <h4 className="py-4 text-teal-600">Desing tools i use</h4>
-              <p className="text-gray-800 py-1">JavaScript</p>
-              <p className="text-gray-800 py-1">NodeJs</p>
-              <p className="text-gray-800 py-1">ReactJs</p>
-              <p className="text-gray-800 py-1">SQL</p>
-              <p className="text-gray-800 py-1">HTML</p>
-              <p className="text-gray-800 py-1">CSS</p>  
+              <ul>
+                <li>CSS</li>
+                <li>Tailwin</li>
+                <li>Boostrap</li>
+                <li>HTML</li>
+                <li>Slim</li>
+              </ul> 
             </div>
           </div>
           
